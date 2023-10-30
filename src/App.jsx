e75fdf3d33d2a21800ae076db1b2bac66a1c76c2
tektechnologies <info@tektechnologies.com>
2023-10-30 11:17:38 -0500

    env server working

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './App.css'
 
+let VITE_APP_SERVER = import.meta.env.VITE_APP_SERVER;
+// console.log('server',VITE_APP_SERVER);
+
 class App extends React.Component  {
   
 render(){
