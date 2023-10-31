e5f115cf53d0aed97c129f4429fe52a81229dec0
tektechnologies <info@tektechnologies.com>
2023-10-31 10:49:12 -0500

    add librarys ; ;

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,9 @@ class App extends React.Component {
       //we need to call the server to get cats from the Database
       let results = await axios.get(`${VITE_APP_SERVER}/cats`);
       console.log('what happened! ',results);
+      this.setState({
+        cats: results.data,
+      })
     } catch (error) {
       console.log("we have an error: ", error.response.data);
     }
@@ -35,9 +38,14 @@ class App extends React.Component {
 
   render() {
     // loop through our cats here
+    console.log(this.state.cats);
+       // add a key
+       let cats = this.state.cats.map(cat => (
+        <p key={cat._id}>{cat.name} is {cat.color}</p>
+      ))
     return (
       <>
-        <h1>hello</h1>
+        <h1>{cats}</h1>
       </>
     );
   }
